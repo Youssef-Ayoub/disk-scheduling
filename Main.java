035f8732b31e080d8f956507e102267fe2ae77fc
Momen83 <77742840+Momen83@users.noreply.github.com>
2022-04-20 23:21:40 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,44 +14,159 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("enter the number of requests ahead");
-        int n = input.nextInt() ;
+        System.out.println ( "1- Take input from command line \n" +
+                "2- Take input from file\n"
+         +"3- Take input from GUI \n");
+        int op;
+        op=input.nextInt ();
+        final Integer[] n = {0};
+        final Integer[] head = {0};
+        if(op==1)
+        {
+            System.out.println("enter the number of requests ahead");
+            n[0] = input.nextInt() ;
 
-        System.out.println("enter the head start");
-        int head = input.nextInt() ;
+            System.out.println("enter the head start");
+            head[0] = input.nextInt() ;
 
-        System.out.println("enter the disk queue");
+            System.out.println("enter the disk queue");
 
-        for(int i = 0 ; i < n ; ++i){
-            list.add( input.nextInt() );
+            for( int i = 0; i < n[0]; ++i){
+                list.add( input.nextInt() );
+            }
         }
 
-        FCFS obj1 = new FCFS(n,list,head) ;
-        obj1.operate();
+       else if(op==2)
+        {
+            String path="E:\\compuer science material\\CS-3_S2\\Advanced os\\Assign2\\input.txt";
+            File myObj = new File (path ); // created a file object named myObj
+            String str="";
+            int lineNum=0;
+            try (Scanner MyReader = new Scanner ( myObj )) {
+                while (MyReader.hasNextLine ( )) {
+                    lineNum++;
+                    String info = MyReader.nextLine ( );
+                    if(lineNum==1)
+                    {
+                      n[0] = Integer.parseInt ( info);
+                    }
+                    else if(lineNum==2)
+                    {
+                        head[0] =Integer.parseInt ( info );
+                    }
+                    else{
+                        String temp="";
+                        info+=' ';
+                        for ( int i=0;i<info.length ();i++ )
+                        {
+                            if(info.charAt ( i )==' ')
+                            {
+                                list.add(Integer.parseInt ( temp ));
+                                temp="";
+                            }
+                            else
+                            {
+                               temp+=info.charAt ( i );
+                            }
+                        }
+                    }
+                 // System.out.println ( info );
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace ( );
+            }
+        }
+       else
+        {
 
-        SCAN obj2 = new SCAN(n,list,head,200,false) ;
-        //last parameter is for direction of scan
-        // if direction is true then it will move right first else it will move left first
-        obj2.operate();
+            final int[] tempn = {0};
+            JFrame GUIFrame = new JFrame ( "inputs" );
+            GUIFrame.setLayout ( new FlowLayout (  ) );
+            JLabel numElmeMessage=new JLabel ( "Enter the number of request a head" );
+            JLabel numHeadMessage=new JLabel ( "Enter the head start" );
+            JLabel DiskQueueMessage=new JLabel ( "Enter the disk queues" );
+            JTextField numElmeMessageField =new JTextField (30 );
+            JTextField numHeadField =new JTextField (30 );
+            JTextField DiskQueuesField=new JTextField (30 );
+            JButton sendInputs=new JButton ( "Send" );
+            sendInputs.setPreferredSize ( new Dimension ( 100,50 ) );
+            numElmeMessage.setFont ( new Font("SansSerif", Font.BOLD, 15)  );
+            numHeadMessage.setFont ( new Font("SansSerif", Font.BOLD, 15)  );
+            DiskQueueMessage.setFont ( new Font("SansSerif", Font.BOLD, 15)  );
+            sendInputs.setFont ( new Font("SansSerif", Font.BOLD, 15)  );
+            numElmeMessageField.setFont ( new Font("SansSerif", Font.BOLD, 15)  );
+            numHeadField.setFont ( new Font("SansSerif", Font.BOLD, 15)  );
+            DiskQueuesField.setFont ( new Font("SansSerif", Font.BOLD, 15)  );
 
-        LOOK obj3 = new LOOK(n,list,head,false) ;
-        //last parameter is for direction of scan
-        // if direction is true then it will move right first else it will move left first
-        obj3.operate();
+            GUIFrame.setVisible ( true );
+            GUIFrame.setSize ( 400,400 );
+            GUIFrame.add ( numElmeMessage );
+            GUIFrame.add ( numElmeMessageField );
+            GUIFrame.add ( numHeadMessage );
+            GUIFrame.add ( numHeadField );
+            GUIFrame.add (DiskQueueMessage );
+            GUIFrame.add (DiskQueuesField);
+            GUIFrame.add ( sendInputs );
+            ActionListener al=new ActionListener ( ) {
+                @Override
+                public void actionPerformed ( ActionEvent e ) {
+                  n[0] =Integer.parseInt ( numElmeMessageField.getText ());
+                  head[0] =Integer.parseInt ( numHeadField.getText () );
+                  String info="";
+                  info=DiskQueuesField.getText ();
+                  info+=' ';
+                  String temp="";
+                    for ( int i=0;i<info.length ();i++ )
+                    {
+                        if(info.charAt ( i )==' ')
+                        {
+                            //System.out.println ( temp );
+                            list.add(Integer.parseInt ( temp ));
+                            temp="";
+                        }
+                        else
+                        {
+                            temp+=info.charAt ( i );
+                        }
+                    }
+                    FCFS obj1 = new FCFS( n[0] ,list, head[0] ) ;
+                    obj1.operate();
 
-        OPT obj4 = new OPT(n,list,head) ;
-        obj4.operate();
+                    SCAN obj2 = new SCAN( n[0] ,list, head[0] ,200,false) ;
+                    //last parameter is for direction of scan
+                    // if direction is true then it will move right first else it will move left first
+                    obj2.operate();
+
+                    LOOK obj3 = new LOOK( n[0] ,list, head[0] ,false) ;
+                    //last parameter is for direction of scan
+                    // if direction is true then it will move right first else it will move left first
+                    obj3.operate();
+
+                    OPT obj4 = new OPT( n[0] ,list, head[0] ) ;
+                    obj4.operate();
+                }
+            };
+         sendInputs.addActionListener ( al );
+        }
+       if(op!=3)
+       {
+           FCFS obj1 = new FCFS( n[0] ,list, head[0] ) ;
+           obj1.operate();
+
+           SCAN obj2 = new SCAN( n[0] ,list, head[0] ,200,false) ;
+           //last parameter is for direction of scan
+           // if direction is true then it will move right first else it will move left first
+           obj2.operate();
+
+           LOOK obj3 = new LOOK( n[0] ,list, head[0] ,false) ;
+           //last parameter is for direction of scan
+           // if direction is true then it will move right first else it will move left first
+           obj3.operate();
+
+           OPT obj4 = new OPT( n[0] ,list, head[0] ) ;
+           obj4.operate();
+       }
+
 
     }
 }
-
-
-//38
-//180
-//130
-//10
-//50
-//15
-//190
-//90
-//150
