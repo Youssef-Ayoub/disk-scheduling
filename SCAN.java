a8cb87babe08a6ce5f3c20b7a18996a7b8e6ba9d
Youssef-Ayoub <81822018+Youssef-Ayoub@users.noreply.github.com>
2022-04-20 18:24:32 +0200

    Scan algorithm

---
+++ SCAN.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SCAN {
+    private ArrayList<Integer> queue = new ArrayList<>() ;
+    private int sizeOfQueue ;
+    private int startHead ;
+    private int finalTail ;
+    private int cost;
+    private boolean direction ;
+    private ArrayList<Integer>sequence = new ArrayList<>() ;
+
+    SCAN(int size , ArrayList<Integer>l , int start ,int end, boolean dir ){
+        this.sizeOfQueue = size ;
+        this.queue.addAll(l) ;
+        this.startHead = start ;
+        cost = 0 ;
+        direction = dir ;
+        finalTail = end ;
+    }
+
+    void operate(){
+        queue.add(startHead);
+        Collections.sort(queue);
+        int index = queue.indexOf(startHead) ;
+
+        if(direction){
+            for(int i = index ; i <= sizeOfQueue ; ++i){
+                sequence.add(queue.get(i));
+            }
+            sequence.add(finalTail);
+            index-- ;
+            for(int i = index ; i >= 0 ; --i){
+                sequence.add(queue.get(i));
+            }
+
+        }else {
+            for(int i = index ; i >= 0 ; --i){
+                sequence.add(queue.get(i));
+            }
+            sequence.add(0);
+            index++ ;
+            for(int i = index ; i <= sizeOfQueue ; ++i){
+                sequence.add(queue.get(i));
+            }
+
+        }
+
+        for(int i = 0 ; i < sequence.size() - 1 ; ++i){
+            cost += Math.abs( sequence.get(i) - sequence.get(i+1) ) ;
+        }
+        System.out.println(sequence);
+        System.out.println("cost = " + cost);
+    }
+
+    public ArrayList<Integer> getQueue() {
+        return queue;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
